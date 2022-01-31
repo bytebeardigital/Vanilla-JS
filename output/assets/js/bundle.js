cd6002241d2692fb069bc4ac022be8de84e43b9a
bytebeardigital <hello@bytebeardigital.com>
2022-01-30 22:36:55 -0500

    added incomeTotal function and updated database

--- output/assets/js/bundle.js
+++ output/assets/js/bundle.js
@@ -10,16 +10,34 @@ fetch("https://my-json-server.typicode.com/bytebeardigital/vanilla-js/db").then(
 });
 
 function getFinances(data) {
-  let startData = { ...data
-  };
-  let moneyData = startData.dashboard;
+  // lets get the dashboard data;
+  let dashboardData = data.dashboard; //lets get the FinanceData
 
-  if (typeof moneyData == "finances") {
-    console.log("money here");
+  let financeData = dashboardData.filter(info => info.type == "finances").map(data => {
+    return [income, expense] = data.data;
+  }); //income information [Array]
+
+  let incomeData = income.income;
+  let expenseData = expense.expense; //p element on the board
+
+  var incomeEl = document.getElementById("incomeTotal");
+  var expenseEl = document.getElementById("expenseTotal"); // totalIncome element set to 0 instead of null;
+
+  let totalIncome = 0;
+  let totalExpenses = 0; // loop through incomeData and add the income_price elements together;
+
+  for (i = 0; i < incomeData.length; i++) {
+    totalIncome = incomeData[i].income_price + totalIncome;
   }
 
-  console.log(startData);
-  console.log(moneyData);
+  for (i = 0; i < expenseData.length; i++) {
+    totalExpenses = expenseData[i].income_price = totalExpenses;
+  } // render income_price sum to incomeEl
+
+
+  incomeEl.innerText = "$" + totalIncome;
+  expenseEl.innerText = "$" + totalExpenses;
+  console.log(expenseData);
 }
 
 class Person {
--- src/js/custom.js
+++ src/js/custom.js
@@ -15,12 +15,40 @@ fetch("https://my-json-server.typicode.com/bytebeardigital/vanilla-js/db")
   });
 
 function getFinances(data) {
-  let startData = { ...data };
-  let moneyData = startData.dashboard;
-  if (typeof moneyData == "finances") {
-    console.log("money here");
+  // lets get the dashboard data;
+  let dashboardData = data.dashboard;
+
+  //lets get the FinanceData
+  let financeData = dashboardData
+    .filter((info) => info.type == "finances")
+    .map((data) => {
+      return ([income, expense] = data.data);
+    });
+
+  //income information [Array]
+  let incomeData = income.income;
+  let expenseData = expense.expense;
+
+  //p element on the board
+  var incomeEl = document.getElementById("incomeTotal");
+  var expenseEl = document.getElementById("expenseTotal");
+
+  // totalIncome element set to 0 instead of null;
+  let totalIncome = 0;
+  let totalExpenses = 0;
+
+  // loop through incomeData and add the income_price elements together;
+  for (i = 0; i < incomeData.length; i++) {
+    totalIncome = incomeData[i].income_price + totalIncome;
   }
 
-  console.log(startData);
-  console.log(moneyData);
+  for (i = 0; i < expenseData.length; i++) {
+    totalExpenses = expenseData[i].income_price = totalExpenses;
+  }
+
+  // render income_price sum to incomeEl
+  incomeEl.innerText = "$" + totalIncome;
+  expenseEl.innerText = "$" + totalExpenses;
+
+  console.log(expenseData);
 }
